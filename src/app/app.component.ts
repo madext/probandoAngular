b9b1e050757af4f7bbea27b7ce34406c3bc9d23b
agalopar.mad <agalopar.mad@gmail.com>
2017-01-27 11:40:34 +0100

    inicio_routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ENTES } from './ente-service/datos-ente'
 import { Ente } from './ente-service/ente'
+import { EnteService } from './ente-service/ente.service'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [EnteService]
 })
 export class AppComponent {
+
   titulo: string = 'Entes'; // variable titulo de la web
-  entes: Ente[] = ENTES; // Inicializa datos desde la constante ENTES
+  entes: Ente[]; // Inicializa datos desde la constante ENTES
   enteSeleccionado : Ente; 
+
+  constructor(private enteService: EnteService) { }
+
+  getEntes(): void {
+  this.enteService.leerEntes().then (entes => this.entes = entes);
+  }
+
+  ngOnInit(): void {
+    this.getEntes();
+  }
   // enteSeleccionado = {id: 33,nombre: 'Jose Lopez'};
   // enteSeleccionado = {};
-
-  seleccionar(ente) { 
+  
+  seleccionar(ente: Ente): void { 
   	this.enteSeleccionado = ente;
   }
 
 
-  };
+  }
