e020efba2ddb6435e8e2327bcadb9f3b5c6e32f7
agalopar.mad <agalopar.mad@gmail.com>
2017-01-27 10:39:51 +0100

    capitulo5ok

--- src/app/ente-service/ente.service.ts
+++ src/app/ente-service/ente.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Ente } from './ente'
+import { ENTES } from './datos-ente'
 
 @Injectable()
 export class EnteService {
-
+  
   constructor() { }
-
+	 leerEntes(): Promise<Ente[]> {
+	  	return Promise.resolve(ENTES);
+	 }
 }
+
