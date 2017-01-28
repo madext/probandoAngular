04bcbbd9222775c75fb3fad39a104469c897ff5a
agalopar.mad <agalopar.mad@gmail.com>
2017-01-28 08:44:10 +0100

    ok_mitadCap6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EnteDetalleComponent } from './ente-detalle/ente-detalle.component';
 import { EntesComponent } from './entes/entes.component';
-import { EnteService } from './ente-service/ente.service'
+import { EnteService } from './ente-service/ente.service';
+import { PanelComponent } from './panel/panel.component'
+
+const appRoutes: Routes = [
+  { path: 'ente', component: EntesComponent },
+  { path: '',
+    redirectTo: '/ente',
+    pathMatch: 'full'
+  },
+
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EnteDetalleComponent,
-    EntesComponent
+    EntesComponent,
+    PanelComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
